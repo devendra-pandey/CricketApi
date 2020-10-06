da2541e3097a6e114d6b077319c197d4f5ed10f1
devendra <devendrapandey@d2webtech.com>
2020-10-06 13:36:52 +0530

    scorecard , commentory and match info done

--- main.py
+++ main.py
@@ -14,30 +14,42 @@ def cricket_info():
     result.append(matches)
     return jsonify({'matches': result})
 
-@app.route('/info/<int:mid>' ,methods=['GET'])
+@app.route('/info/<mid>' ,methods=['POST'])
 def match_info(mid):
+    match_info_result = []
     c = Cricbuzz()
     minfo = c.matchinfo(mid)
     print(json.dumps(minfo, indent=4, sort_keys=True))
+    match_info_result.append(minfo)
+    return jsonify({'match information':match_info_result})
 
-@app.route('/live/<int:mid>',methods=['GET'])
+@app.route('/live/<mid>',methods=['POST'])
 def live_score(mid):
+    match_live_score = []
     c = Cricbuzz()
     lscore = c.livescore(mid)
     print("***********")
     print(json.dumps(lscore, indent=4, sort_keys=True))
+    match_live_score.append(lscore)
+    return jsonify({'Live score ': match_live_score})
 
-@app.route('/commentory/<int:mid>',methods=['GET'])
+@app.route('/commentory/<mid>',methods=['POST'])
 def commentary(mid):
+    commentory_by_ball = []
     c = Cricbuzz()
     comm = c.commentary(mid)
     print(json.dumps(comm, indent=4, sort_keys=True))
+    commentory_by_ball.append(comm)
+    return jsonify({'commentory by Ball': commentory_by_ball})
 
-@app.route('/scorecard/<int:mid>',methods=['GET'])
+@app.route('/scorecard/<mid>',methods=['POST'])
 def scorecard(mid):
+    result = []
     c = Cricbuzz()
     scard = c.scorecard(mid)
     print(json.dumps(scard, indent=4, sort_keys=True))
+    result.append(scard)
+    return jsonify({'scorecard' : result })
 
 
 if __name__ == '__main__':
